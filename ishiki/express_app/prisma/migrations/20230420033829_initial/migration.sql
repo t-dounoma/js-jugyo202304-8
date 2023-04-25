e988a9f149e6263733782560dcea586da1c4572f
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-04-25 09:14:01 +0900

    prisma環境構築

---
+++ ishiki/express_app/prisma/migrations/20230420033829_initial/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `age` INTEGER NOT NULL DEFAULT 0,
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
