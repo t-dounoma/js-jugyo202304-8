d8f27d12c914ae68b07f19dcad38e7888257ee26
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-04-27 10:54:37 +0900

    編集機能作成

--- ishiki/express_app/routes/db.js
+++ ishiki/express_app/routes/db.js
@@ -13,10 +13,28 @@ router.get('/', async function (req, res, next) {
   };
   res.render('db', data);
 });
-
 router.post('/', async function (req, res, next) {
   const { name, email, age } = req.body;
   await prisma.user.create({
+    data: { name: name, email: email, age: parseInt(age) },
+  });
+  res.redirect('/db');
+});
+
+router.get('/edit/:id', async function (req, res, next) {
+  const { id } = req.params;
+  const user = await prisma.user.findFirst({ where: { id: parseInt(id) } });
+  const data = {
+    title: 'Prisma',
+    message: 'ID = ' + id + ' のデータの更新。',
+    user: user,
+  };
+  res.render('edit', data);
+});
+router.post('/edit', async function (req, res, next) {
+  const { id, name, email, age } = req.body;
+  await prisma.user.update({
+    where: { id: parseInt(id) },
     data: { name: name, email: email, age: parseInt(age) },
   });
   res.redirect('/db');
