684cb8653b7fe7f7e25bf915d1dbf00323a515ec
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-25 11:36:45 +0900

    session の利用

--- ishiki/adonis_app/app/Controllers/Http/SamplesController.ts
+++ ishiki/adonis_app/app/Controllers/Http/SamplesController.ts
@@ -2,19 +2,28 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class SamplesController {
   public async index(ctx: HttpContextContract) {
-    const msg =
-      "ID:" + ctx.request.param("id") + ", pass:" + ctx.request.param("pass");
+    var list = ctx.session.get("list");
+    if (list == undefined) {
+      list = [];
+      ctx.session.put("list", list);
+    }
     const data = {
       title: "Sample",
-      message: msg,
+      message: "メッセージを送信：",
+      list: list,
     };
     return ctx.view.render("samples/index", data);
   }
 
   public async index_posted(ctx: HttpContextContract) {
+    const list = ctx.session.get("list");
+    const msg = ctx.request.input("msg");
+    list.unshift(ctx.request.input("msg"));
+    ctx.session.put("list", list);
     const data = {
       title: "Sample",
-      message: JSON.stringify(ctx.request.body()),
+      message: "「" + msg + "」を送信しました。",
+      list: list,
     };
     return ctx.view.render("samples/index", data);
   }
