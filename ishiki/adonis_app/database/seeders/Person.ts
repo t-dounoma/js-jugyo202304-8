07f7201d542657b1ae31e501b01887fc7acc9e84
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-30 13:54:22 +0900

    シードファイルの実行

---
+++ ishiki/adonis_app/database/seeders/Person.ts
@@ -0,0 +1,24 @@
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import Person from "App/Models/Person";
+
+export default class PersonSeeder extends BaseSeeder {
+  public async run() {
+    await Person.createMany([
+      {
+        name: "taro",
+        email: "taro@yamada",
+        age: 39,
+      },
+      {
+        name: "hanako",
+        email: "hanako@flower",
+        age: 28,
+      },
+      {
+        name: "sachiko",
+        email: "sachiko@happy",
+        age: 17,
+      },
+    ]);
+  }
+}
