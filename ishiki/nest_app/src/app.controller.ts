e590cdeea981b25735a0297788942e0e36003a57
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-06 14:49:09 +0900

    Webページからバックエンドにアクセスする

--- ishiki/nest_app/src/app.controller.ts
+++ ishiki/nest_app/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/hello/:id?')
+  getHello(@Param() params): object {
+    const id = params.id ?? 0;
+    return this.appService.getHello(id);
   }
 }
