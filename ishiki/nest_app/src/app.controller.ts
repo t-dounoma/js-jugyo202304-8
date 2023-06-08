ceb6733f2697b913a1d654679d733ec3975738dc
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-08 11:38:48 +0900

    data追加処理

--- ishiki/nest_app/src/app.controller.ts
+++ ishiki/nest_app/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import FormData from './formdata';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,11 @@ export class AppController {
   getHello(@Param() params): object {
     const id = params.id ?? 0;
     return this.appService.getHello(id);
+  }
+
+  @Post('/hello/post')
+  post(@Body() frm: FormData): string {
+    this.appService.addData(frm);
+    return 'form data was pushed!';
   }
 }
