25bb597d5b88f3438f4dd61e8bd589e43917120b
t-dounoma <101164515+t-dounoma@users.noreply.github.com>
2023-06-13 13:56:47 +0900

    [feat] 0613
    - nestJS

--- dounoma/adonis_app/app/Controllers/Http/PeopleController.ts
+++ dounoma/adonis_app/app/Controllers/Http/PeopleController.ts
@@ -1,65 +1,88 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import Person from "App/Models/Parson";
 
 
-
 export default class PeopleController {
-  public async index(ctx:HttpContextContract){
-    const people = await Person.all()
+  public async index(ctx: HttpContextContract) {
+    const qs = ctx.request.qs()
+    var p = parseInt(qs.page ? qs.page : 1)
+    p = p < 1 ? 1 : p
+    const people=await Person.query().paginate(p,5)
     const data = {
-      title:"sample",
-      message:"Lucid",
-      data:people
+      title: "sample",
+      message: "Lucid",
+      page:p,
+      data: people
     }
-    return ctx.view.render("people/index",data)
-  }
-  public async add(ctx:HttpContextContract){
-    const data = {
-      title:"Add",
-      message:"Personの新規作成"
-    }
-    return ctx.view.render("people/add",data)
+    return ctx.view.render("people/index", data)
   }
 
-  public async add_posted(ctx:HttpContextContract){
+  public async add(ctx: HttpContextContract) {
+    const data = {
+      title: "Add",
+      message: "Personの新規作成"
+    }
+    return ctx.view.render("people/add", data)
+  }
+
+  public async add_posted(ctx: HttpContextContract) {
     await Person.create(ctx.request.body())
     return ctx.response.redirect("/person")
   }
-  public async edit(ctx:HttpContextContract){
+
+  public async edit(ctx: HttpContextContract) {
     const id = ctx.request.param("id")
     const person = await Person.find(id)
     const data = {
-      title:"Edit",
-      message:"Person id=" + id + "の編集",
-      person:person
+      title: "Edit",
+      message: "Person id=" + id + "の編集",
+      person: person
     }
-    return ctx.view.render("people/edit",data)
+    return ctx.view.render("people/edit", data)
   }
 
-  public async edit_posted(ctx:HttpContextContract){
+  public async edit_posted(ctx: HttpContextContract) {
     const id = ctx.request.param("id")
     const person = await Person.find(id)
     await person?.merge(ctx.request.body()).save()
     return ctx.response.redirect("/person")
   }
 
-  public async delete(ctx:HttpContextContract){
+  public async delete(ctx: HttpContextContract) {
     const id = ctx.request.param("id")
     const person = await Person.find(id)
     const data = {
-      title:"Delete",
-      message:"Person id=" + id + "の削除します",
-      person:person
+      title: "Delete",
+      message: "Person id=" + id + "の削除します",
+      person: person
     }
-    return ctx.view.render("people/delete",data)
+    return ctx.view.render("people/delete", data)
   }
 
-  public async delete_posted(ctx:HttpContextContract){
+  public async delete_posted(ctx: HttpContextContract) {
     const id = ctx.request.param("id")
     const person = await Person.find(id)
     await person?.delete()
     return ctx.response.redirect("/person")
   }
 
+  public async find(ctx: HttpContextContract) {
+    var find: string
+    var people: Person[]
 
+    if (ctx.request.method() == "POST") {
+      find = ctx.request.input("find")
+      people = await Person.query().where("name", "like", "%" + find + "%").orderBy("age", "desc").exec()
+    } else {
+      find = ""
+      people = await Person.query().orderBy("age", "desc").exec()
+    }
+    const data = {
+      title: "Find",
+      message: "",
+      find: find,
+      data: people
+    }
+    return ctx.view.render("people/find", data)
+  }
 }
